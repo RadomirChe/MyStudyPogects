6c154cd1761556f3f460b7bfe6d168dd3e7bd059
RadomirChe <radocalipsis@gmail.com>
2023-04-05 17:34:21 +0200

    update 1 and 2 HW

--- HW1.py
+++ HW1.py
@@ -1,2 +1,2 @@
-list = ['Baksik', 'Murzik', 'Markiz']
-print(list[0], list[1], list[2], sep=', ')
+list1 = ['Baksik', 'Murzik', 'Markiz']
+print(list1[0], list1[1], list1[2], sep=', ')
--- HW2.py
+++ HW2.py
@@ -1,3 +1,4 @@
 list1 = ['Ukraine', 'Spain', 'Italy']
 dict_sample = {list1[0]: 'Kyiv', list1[1]: 'Madrid', list1[2]: 'Rome'}
-for key, value in dict_sample.items(): print(key,value, sep=': ')
+for key, value in dict_sample.items():
+    print(key, value, sep=': ')
